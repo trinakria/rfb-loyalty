c03139a741f5e7571174349d49a21be0bdaaf9f9
trinakria <mario.giurlanda@gmail.com>
2018-04-01 13:27:39 +0200

    release plugin and configuration

--- webpack/utils.js
+++ webpack/utils.js
@@ -9,9 +9,9 @@ module.exports = {
 
 // Returns the second occurrence of the version number from `build.gradle` file
 function parseVersion() {
-    const versionRegex = /^version\s*=\s*[',"]([^',"]*)[',"]/gm; // Match and group the version number
-    const buildGradle = fs.readFileSync('build.gradle', 'utf8');
-    return versionRegex.exec(buildGradle)[1];
+    const versionRegex = /^version\s*=\s*(.*)/gm; // Match and group the version number
+    const buildGradle = fs.readFileSync('gradle.properties', 'utf8');
+    return versionRegex.exec(buildGradle)[0];
 }
 
 const _root = path.resolve(__dirname, '..');
